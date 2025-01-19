2dfcffec8d5f9eb3d427b912282989e5e71b516b
cueboid <cubeyyyy@pm.me>
2025-01-19 06:56:44 +0000

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://the-based-smp.github.io',
+  integrations: [sitemap()],
 });
